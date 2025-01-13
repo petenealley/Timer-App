a9c189194707c867dbf93a8857bcd59af5180880
petenealley <34818863+petenealley@users.noreply.github.com>
2025-01-12 21:32:47 -0500

    created TimerObject

---
+++ Timer App/TimerObject.swift
@@ -0,0 +1,70 @@
+//
+//  TimerObject.swift
+//  Timer App
+//
+//  Created by Pete Nealley on 1/12/25.
+//
+
+import SwiftUI
+
+@Observable
+class TimerObject {
+    let timerColor: Color
+    let length: Int
+    
+    init(timerColor: Color, length: Int) {
+        self.timerColor = timerColor
+        self.length = length
+    }
+    
+    var timer: Timer? = nil
+    var timeElapsed = 0
+    
+    var isTimerRunning = false
+    
+    var remainingTime: Int {
+        length - timeElapsed
+    }
+    
+    var progress: CGFloat {
+        CGFloat(length - remainingTime) / CGFloat(length)
+    }
+    
+    func startTimer() {
+        isTimerRunning = true
+        timer = Timer.init(timeInterval: 1, repeats: true) { [self] _ in
+            if remainingTime > 0 {
+                timeElapsed += 1
+            } else {
+                stopTimer()
+            }
+        }
+    }
+    
+    func stopTimer() {
+        if isTimerRunning {
+            isTimerRunning = false
+            timer?.invalidate()
+        }
+    }
+    
+    func reserTimer(){
+        timeElapsed = 0
+        isTimerRunning = false
+    }
+    
+    var playButtonDisabled: Bool {
+        guard remainingTime > 0, !isTimerRunning else { return true }
+        return false
+    }
+    
+    var pauseButtonDisabled: Bool {
+        guard remainingTime > 0, isTimerRunning else { return true }
+        return false
+    }
+    
+    var resetButtonDisabled: Bool {
+        guard remainingTime != length, !isTimerRunning else { return true }
+        return false
+    }
+}
