e913f7407966061795af59529c4499add0a168e4
petenealley <34818863+petenealley@users.noreply.github.com>
2025-01-12 20:53:55 -0500

    Initial Commit

---
+++ Timer App/Timer_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Timer_AppApp.swift
+//  Timer App
+//
+//  Created by Pete Nealley on 1/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Timer_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
